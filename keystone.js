138c1327610da6ea2caf4b2d368b0859ec92ac47
Paul Giron <pol.giron@gmail.com>
2018-05-25 12:11:45 +1200

    First commit

---
+++ keystone.js
@@ -0,0 +1,41 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+var cons = require('consolidate');
+var nunjucks = require('nunjucks');
+
+keystone.init({
+	'name': 'pj',
+	'brand': 'pj',
+
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': '.html',
+	'custom engine': cons.nunjucks,
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	users: 'users',
+});
+
+
+
+keystone.start();
